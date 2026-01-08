63740684100bd360ab34dbd2abd3ee33a9cd9172
lukew3 <lukew25073@gmail.com>
2026-01-08 05:01:13 +0000

    Deploying to gh-pages from @ lukew3/wordcollector@100d50d0aa41a3fdd5c688bf93b020544b1276be 🚀

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'offline-dictionary-v1';
+const CACHE_NAME = 'offline-dictionary-v2';
+const DB_CACHE_NAME = 'offline-dictionary-db-v1';
+
 const urlsToCache = [
   '/',
   '/static/js/bundle.js',
@@ -6,21 +8,78 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
+const dbUrlsToCache = [
+  '/wordnetFull.db'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    Promise.all([
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(urlsToCache)),
+      caches.open(DB_CACHE_NAME)
+        .then((cache) => cache.addAll(dbUrlsToCache))
+    ])
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME && cacheName !== DB_CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (url.pathname === '/wordnetFull.db') {
+    event.respondWith(
+      caches.open(DB_CACHE_NAME)
+        .then((dbCache) => {
+          return dbCache.match(request)
+            .then((cachedResponse) => {
+              if (cachedResponse) {
+                return fetch(request)
+                  .then((networkResponse) => {
+                    if (networkResponse.ok) {
+                      dbCache.put(request, networkResponse.clone());
+                      return networkResponse;
+                    }
+                    return cachedResponse;
+                  })
+                  .catch(() => cachedResponse);
+              }
+              
+              return fetch(request)
+                .then((networkResponse) => {
+                  if (networkResponse.ok) {
+                    dbCache.put(request, networkResponse.clone());
+                  }
+                  return networkResponse;
+                });
+            });
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(request);
+        })
+    );
+  }
 });
